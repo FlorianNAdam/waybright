e88b60c4079e39a57ed0f28e1438044ae384cb9f
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2026-08-10 22:09:27 +0200

    better nixos module

--- nix/module.nix
+++ nix/module.nix
@@ -7,56 +7,73 @@
   ...
 }:
 let
-  cfg = config.services.waybright.waydark;
+  cfg = config.services.waybright;
+  waydark = cfg.waydark;
   system = pkgs.stdenv.hostPlatform.system;
 in
 {
-  options.services.waybright.waydark = {
-    enable = lib.mkEnableOption "waydark software dimming daemon";
+  options.services.waybright = {
+    enable = lib.mkEnableOption "waybright brightness tools";
 
     package = lib.mkOption {
       type = lib.types.package;
-      default = self.packages.${system}.waydark;
-      defaultText = lib.literalExpression "waybright.packages.\${pkgs.stdenv.hostPlatform.system}.waydark";
-      description = "The waydark package to use.";
+      default = self.packages.${system}.waybright;
+      description = "The waybright package to install.";
     };
 
-    socketPath = lib.mkOption {
-      type = lib.types.str;
-      default = "%t/waydark.sock";
-      description = ''
-        Socket path for the waydark daemon. The default expands to
-        $XDG_RUNTIME_DIR/waydark.sock in the user systemd manager.
-      '';
-    };
-  };
+    waydark = {
+      enable = lib.mkEnableOption "waydark software dimming daemon";
 
-  config = lib.mkIf cfg.enable {
-    systemd.user.sockets.waydark = {
-      description = "waydark daemon socket";
-      wantedBy = [ "sockets.target" ];
-
-      socketConfig = {
-        ListenStream = cfg.socketPath;
-        RemoveOnStop = true;
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = self.packages.${system}.waydark;
+        description = "The waydark package to use.";
       };
-    };
 
-    systemd.user.services.waydark = {
-      description = "waydark software dimming daemon";
-      wantedBy = [ "graphical-session.target" ];
-      requires = [ "waydark.socket" ];
-      after = [
-        "graphical-session.target"
-        "waydark.socket"
-      ];
-      partOf = [ "graphical-session.target" ];
-
-      serviceConfig = {
-        ExecStart = "${cfg.package}/bin/waydark daemon";
-        Environment = [ "WAYDARK_SOCKET=${cfg.socketPath}" ];
-        Restart = "on-failure";
+      socketPath = lib.mkOption {
+        type = lib.types.str;
+        default = "%t/waydark.sock";
+        description = ''
+          Socket path for the waydark daemon. The default expands to
+          $XDG_RUNTIME_DIR/waydark.sock in the user systemd manager.
+        '';
       };
     };
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      environment.systemPackages = [ cfg.package ];
+      services.waybright.waydark.enable = lib.mkDefault true;
+    })
+
+    (lib.mkIf waydark.enable {
+      systemd.user.sockets.waydark = {
+        description = "waydark daemon socket";
+        wantedBy = [ "sockets.target" ];
+
+        socketConfig = {
+          ListenStream = waydark.socketPath;
+          RemoveOnStop = true;
+        };
+      };
+
+      systemd.user.services.waydark = {
+        description = "waydark software dimming daemon";
+        wantedBy = [ "graphical-session.target" ];
+        requires = [ "waydark.socket" ];
+        after = [
+          "graphical-session.target"
+          "waydark.socket"
+        ];
+        partOf = [ "graphical-session.target" ];
+
+        serviceConfig = {
+          ExecStart = "${waydark.package}/bin/waydark daemon";
+          Environment = [ "WAYDARK_SOCKET=${waydark.socketPath}" ];
+          Restart = "on-failure";
+        };
+      };
+    })
+  ];
 }
